be8763e5f770fcb92cb09f28a811ef7422e9706c
yaminiprasanna <34371375+yaminiprasanna@users.noreply.github.com>
2020-11-13 09:53:51 +0530

    Delete Person.js

--- src/components/Person.js
+++
@@ -1,8 +0,0 @@
-import React,{Component} from 'react'
-function Person({person,key}){
-    return(
-        <div>
-            <h2>Iam {person.name} with age{person.age} and my id is {person.id}</h2> </div>
-    )
-}
-export default Person
